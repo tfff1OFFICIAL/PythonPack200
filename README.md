bb98202169d65bb5ac17d6a38706aef04ce0fcf2
Tfff1OFFICIAL <tfff1OFFICIAL@users.noreply.github.com>
2018-04-16 11:54:18 +1200

    updated README.md and setup.py

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# PythonPack200
-my attempt at implemented Pack200 (UnPack200 first) in a Python-accessible way
+# PythonUnpack200
+A Python wrapper for the JDK unpack200 utility.
+
+## Usage
+
+The unpack200.unpack function takes three parameters:
+* infile: string, full path to the pack200 file to extract
+* outfile: string, full path to the jar file you'd like to extract to
+* remove_source: bool (optional, default False), whether to remove the pack200 file specified in <infile> after extraction is complete
+
+For example:
+```python
+import unpack200
+
+unpack200.unpack(
+	r"C:\path\to\packfile",
+	r"C:\path\to\outfile"
+)
+```
+
+## Building
+
+Setup.py is currently only set up for Windows at the moment - feel free to contribute a Linux or Mac PR!
+
+To build, first download and extract the following dependencies to your PC:
+* Java JDK source code, e.g. from: https://github.com/dmlloyd/openjdk
+* Java JDK built
+
+run:
+```bash
+python3 setup.py build
+```
+
+It will ask you two questions, enter a full path into both, e.g:
+```bash
+Where is the JDK source located? C:\JDK\src
+Where is the JDK include located? C:\Program Files\Java\jdk1.8.0_131\include
+```
+It will attempt to derive the correct locations for everything from these paths, and should build the extension.
+
+## Issues
+
+Found an issue? Please report it either in the issues section of this repo, or to the developers of JDK. Any fix PRs would be welcome!
--- setup.py
+++ setup.py
@@ -1,6 +1,51 @@
 from os.path import join
 from setuptools import setup, Extension
 
+LONG_DESC = """# PythonUnpack200
+A Python wrapper for the JDK unpack200 utility.
+
+## Usage
+
+The unpack200.unpack function takes three parameters:
+* infile: string, full path to the pack200 file to extract
+* outfile: string, full path to the jar file you'd like to extract to
+* remove_source: bool (optional, default False), whether to remove the pack200 file specified in <infile> after extraction is complete
+
+For example:
+```python
+import unpack200
+
+unpack200.unpack(
+	r"C:\path\to\packfile",
+	r"C:\path\to\outfile"
+)
+```
+
+## Building
+
+Setup.py is currently only set up for Windows at the moment - feel free to contribute a Linux or Mac PR!
+
+To build, first download and extract the following dependencies to your PC:
+* Java JDK source code, e.g. from: https://github.com/dmlloyd/openjdk
+* Java JDK built
+
+run:
+```bash
+python3 setup.py build
+```
+
+It will ask you two questions, enter a full path into both, e.g:
+```bash
+Where is the JDK source located? C:\JDK\src
+Where is the JDK include located? C:\Program Files\Java\jdk1.8.0_131\include
+```
+It will attempt to derive the correct locations for everything from these paths, and should build the extension.
+
+## Issues
+
+Found an issue? Please report it either in the issues section of this repo, or to the developers of JDK. Any fix PRs would be welcome!
+"""
+
 JDK_source_path = input("Where is the JDK source located? ")
 JDK_include_path = input("Where is the JDK include located? ")
 
@@ -8,6 +53,21 @@ unpack_natives_path = join(JDK_source_path, "jdk.pack", "share", "native")
 
 setup(
     name="unpack200",
+	author="tfff1",
+	license="MIT",
+	description="A Python wrapper for the JDK unpack200 utility",
+	long_description=LONG_DESC,
+	long_description_content_type="text/markdown",
+	classifiers=[
+		'License :: OSI Approved :: MIT License',
+		"Programming Language :: Python :: 3.6"
+	],
+	keywords="pack pack200 java packfile",
+	url="https://github.com/tfff1OFFICIAL/PythonUnpack200/",
+	project_urls={
+		"Source": "https://github.com/tfff1OFFICIAL/PythonUnpack200/"
+	},
+	python_requires=">=3",
     version="1.0",
     ext_modules=[Extension(
         "unpack200",
